d38dc3c55f80965fc632cc4cca9bfe20cdd6d4fd
Isaac Freires <isaac.freires@osf.digital>
2023-04-10 00:04:12 -0300

    Adicionado construtores; Atributo estático; Getters e Setters. Refatoração do código; NPM Start

--- Bytebank_javascript/ContaCorrente.js
+++ Bytebank_javascript/ContaCorrente.js
@@ -1,7 +1,32 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
+    static numeroDeContas = 0; // atributo estático, serve como uma variável global
     agencia;
+    _cliente;
+
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
     // #saldo = 0 - # torna o atributo privado - https://github.com/tc39/proposal-class-fields#private-fields
     _saldo = 0; // _ torna o atributo privado, porém não faz com que seja realmente privado
+
+    get saldo (){
+        return this._saldo;
+    }
+
+    constructor(agencia, cliente){
+        this.agencia = agencia;
+        this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
@@ -15,5 +40,10 @@ export class ContaCorrente {
             return;
         } // early return - https://www.lucaspaganini.com/pt/academy/early-returns-code-patterns-in-javascript#early-exits
         this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valor);
     }
 }
--- Bytebank_javascript/index.js
+++ Bytebank_javascript/index.js
@@ -2,21 +2,26 @@ import { Cliente } from "./Cliente.js";
 import { ContaCorrente } from "./ContaCorrente.js";
 
 
-const cliente1 = new Cliente();
-cliente1.nome = "Isaac";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Isaac", 11122233309); // instanciando um objeto da classe Cliente
+const cliente2 = new Cliente("Freires", 88822233309);
 
-const cliente2 = new Cliente();
-cliente2.nome = "Freires";
-cliente2.cpf = 88822233309;
-
-const contaCorrenteIsaac = new ContaCorrente();
-contaCorrenteIsaac.agencia = 1001;
+const contaCorrenteIsaac = new ContaCorrente(1001, cliente1);
 
 contaCorrenteIsaac.depositar(200);
-contaCorrenteIsaac.depositar(100);
 
-const valorSacado = contaCorrenteIsaac.sacar(50);
-console.log(valorSacado);
+const conta2 = new ContaCorrente(102, cliente2);
 
+let valor = 50;
+contaCorrenteIsaac.transferir(valor, conta2);
+
+console.log("Cliente 1:");
+console.log(cliente1);
+
+console.log(".......................");
+
+console.log("Conta Corrente do Cliente 1:");
 console.log(contaCorrenteIsaac);
+
+console.log(".......................");
+
+console.log("Número de Contas: " + ContaCorrente.numeroDeContas);
